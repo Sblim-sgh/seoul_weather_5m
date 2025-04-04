69382a1311d131f96615830f4c15a370b552ee1e
Sblim-sgh <sharkroo@gmail.com>
2025-04-04 12:20:28 +0900

    Update seoul_weather_5m.py

--- seoul_weather_5m.py
+++ seoul_weather_5m.py
@@ -1,39 +1,35 @@
-#서울 기온 정보를 5분마다 수집하여 csv로 저장
+#5분마다 한 번씩 서울의 기온 정보를 csv형태로 저장!!
+import requests
+import csv
+from datetime import datetime
+import os
 
-name : Fetch Seoul Weather Data per 5m
+MY_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+CITY="Seoul"
+URL=f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={MY_API_KEY}&units=metric"
+response=requests.get(URL)
+data=response.json()
+#서울 기온 하나
+temp=data["main"]["temp"]
+time=datetime.now().strftime("%Y-%m-%d%H:%M:%S")
+csv_filename="seoul_weather.csv"
+header=["time", "temp"]
 
-on: 
-  schedule:
-    - cron: "*/5 * * * *"
-  workflow_dispatch:
-jobs:
-  fetch_weather_seoul_5m:
-    runs-on: ubuntu-latest
+#seoul_weather.csv 가없으면 새로 생성
+#만약 있따면 갱신
+file_exist=os.path.isfile(csv_filename)
 
-    steps:
-      - name: 저장소 체크아웃
-        uses: actions/checkout@v3 #현재 git 저장소 가져오기
-        with:
-         token: ${{secrets.GITHUB_TOKEN}}
-      - name: Python 설정
-        uses: actions/setup-python@v4
-        with:
-          python-version: "3.9"
+#a mode: 없으면 write, 있으면 불러오기
+#w mode: 무조건 덮어쓰기
+with open(csv_filename,"a", newline="") as file:
+    writer = csv.writer(file)
+    
+    if not file_exist:
+        writer.writerow(header)
 
-      - name: 필요한 패키지 설치
-        run: pip install requests
+    writer.writerow([temp, time])
 
-      - name: 날씨 데이터 가져오기
-        env:
-          OPENWEATHER_API_KEY: ${{secrets.OPENWEATHER_API_KEY}}
-        run: python seoul_weather_5m.py
+    print("서울 기온 저장 완료!!")
 
-      - name: 변경 사항 커밋 및 푸시
-        run: |
-          git config --global user.name "github-actions[bot]"
-          git config --global user.email "github-actions@github.com"
-          git add seoul_weather.csv
-          git commit -m "Update weather data (auto)"
-          git push 
-         
-  
+
+
